57747ca7336344035ccee444d6ba91f5a396ddaa
alexykn <alexander.knott@posteo.de>
2025-02-04 19:53:37 +0100

    feat: added cuda support - experimental, can't test on my mac

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,24 +1,28 @@
 import warnings
-from kokoro import KPipeline
+from kokoro import KPipeline, KModel
 from rich.console import Console
 from rich import print as rprint
 import uvicorn
-
+import torch
 warnings.filterwarnings("ignore", category=FutureWarning, module="torch.nn.utils.weight_norm")
 warnings.filterwarnings("ignore", category=UserWarning, module="torch.nn.modules.rnn")
 
 console = Console()
-device = 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+model = KModel().to(device).eval()
 
 # Initialize pipelines for both languages
 pipelines = {
-    'a': KPipeline(lang_code='a'),  # American English
-    'b': KPipeline(lang_code='b')   # British English
+    'a': KPipeline(lang_code='a', model=model),  # American English
+    'b': KPipeline(lang_code='b', model=model)   # British English
 }
 
 if __name__ == "__main__":
     try:
         from api.api import app
+        rprint(f"[green]INFO:     Using device: {model.device}[/green]")
+
         # Start the FastAPI server
         rprint("[green]INFO:     OK[/green]")
         uvicorn.run(app, host="0.0.0.0", port=5005)
